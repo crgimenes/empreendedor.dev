741db1e7274dc3408c652cb572ac5c1cfe8f1032
crgimenes <crg@crg.eti.br>
2025-09-11 00:46:51 -0300

    update init.lua example

--- sample_init.lua
+++ sample_init.lua
@@ -5,6 +5,22 @@ if _G.GitTag == nil then
     GitTag = ""
 end
 
-print("Version: " .. GitTag)
+if _G.BaseURL == nil then
+    BaseURL = "http://localhost:3210"
+end
+
+local function getEnv(name, default)
+    local value = os.getenv(name)
+    if value == nil then
+        return default
+    end
+    return value
+end
+
 Address = ":3210"
+GitHubClientID = getEnv("GITHUB_CLIENT_ID", "")
+GitHubClientSecret = getEnv("GITHUB_CLIENT_SECRET", "")
+
+print("Version: " .. GitTag)
+print("BaseURL: " .. BaseURL)
 
