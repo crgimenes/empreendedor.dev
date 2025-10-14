cc05043210adb7d0ebd8e66743fecb0dfe25da77
crgimenes <crg@crg.eti.br>
2025-10-13 23:25:40 -0300

    remove down migration (doesn't make sense with sqlite)

--- migration/001_base_system.down.sql
+++
@@ -1,8 +0,0 @@
--- Drop tables if they exist (SQLite)
--- Note: SQLite does not support CASCADE in DROP TABLE; foreign keys are enforced on DELETE only.
-DROP TRIGGER IF EXISTS identities_set_updated_at;
-DROP TRIGGER IF EXISTS users_set_updated_at;
-DROP INDEX IF EXISTS idx_identities_user_id;
-DROP TABLE IF EXISTS identities;
-DROP TABLE IF EXISTS users;
-
